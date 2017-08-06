907951072a23d952944be76bc22ed42bf49e5c36
Lucia Espona <lucia.espona@wsl.ch>
2017-08-06 22:19:54 +0200

    implemented delta import

--- ckanext/oaipmh_repository/plugin.py
+++ ckanext/oaipmh_repository/plugin.py
@@ -2,6 +2,9 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 import urllib
+import time
+
+from multiprocessing import Process
 
 from pylons import config
 
@@ -29,11 +32,22 @@ class Oaipmh_RepositoryPlugin(plugins.SingletonPlugin):
 
     # IPackageController
     def before_index(self, pkg_dict):
-        log.debug(' *** BEFORE INDEX ***')
+        log.debug(' *** BEFORE INDEX PKG ***')
+        self.async_solr_update()
+        return pkg_dict
+
+    def async_solr_update(self):
+        process = Process(target=self.update_solr_DIH, args=(60,))
+        process.daemon = True
+        process.start()
+
+   # Update Solr Delta import
+    def update_solr_DIH(self, delay=10):
         doi_solr_url = config.get('oaipmh_repository.solr_url')
         if doi_solr_url:
             url_delta = doi_solr_url + '/dataimport?command=delta-import&clean=false&wt=json'
-            log.debug('Calling delta-import: {0} '.format(url_delta))
+            log.debug('Calling delta-import (delay={0}s): {1} '.format(delay, url_delta))
+            time.sleep(delay)
             fileobj = urllib.urlopen(url_delta)
-            log.debug(fileobj.read())
-        return pkg_dict
+            log.debug('Delta import DONE: {0}'.format(fileobj.read()))
+
